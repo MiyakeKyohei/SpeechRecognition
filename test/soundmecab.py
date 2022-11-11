46bdfbc4f34e6214959202429034e06ec49cdc6c
KizukuM <109737009+KizukuM@users.noreply.github.com>
2022-11-11 16:39:59 +0900

    addict new code

---
+++ test/soundmecab.py
@@ -0,0 +1,42 @@
+import os
+import MeCab
+
+import speech_recognition as sr
+from pykakasi import kakasi
+
+cmd_file = "0.bat"   # .cmdファイルへのパス
+command = cmd_file
+
+listener = sr.Recognizer()
+
+try:
+    with sr.Microphone() as source:
+        print("Listening...")
+        voice = listener.listen(source)
+        voice_text = listener.recognize_google(voice, language="ja-JP")
+        print(voice_text)
+
+        mecab= MeCab.Tagger()
+        node = mecab.parseToNode(voice_text)  
+        
+        kakasi = kakasi()
+
+        while node:
+            
+            print(node.surface)
+            
+            result = kakasi.convert(node.surface)
+            print(result)
+            
+            text = result[0]['hira']
+            print(text)
+            if text == 'わたなべ':
+                print('collect')
+                os.system(command)
+                break
+ 
+            else:node = node.next
+ 
+except:
+    print('sorry I could not listen')
+    
